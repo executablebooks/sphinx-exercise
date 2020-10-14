de2f98958cf799a94e1713f948433fc9306f70a1
najuzilu <junaluzi@gmail.com>
2020-10-14 14:28:46 +0200

    :sparkles: NEW: hide specific directives and hide all solutions

--- sphinx_exercise/directive.py
+++ sphinx_exercise/directive.py
@@ -19,48 +19,48 @@ from sphinx.util import logging
 logger = logging.getLogger(__name__)
 
 
-class ExerciseDirective(SphinxDirective):
-    """ A custom Sphinx Directive """
+class CustomDirective(SphinxDirective):
+    """ A custom Sphinx directive """
 
-    name = "exercise"
-    has_content = True
-    required_arguments = 0
-    optional_arguments = 1
-    final_argument_whitespace = True
-    option_spec = {
-        "label": directives.unchanged_required,
-        "class": directives.class_option,
-        "nonumber": directives.flag,
-    }
+    name = ""
 
     def run(self) -> List[Node]:
+        if self.name == "solution" and self.env.app.config.hide_solutions:
+            return []
+
         serial_no = self.env.new_serialno()
 
         if not hasattr(self.env, "exercise_list"):
             self.env.exercise_list = {}
 
-        # Take care of class option
         classes, class_name = [self.name], self.options.get("class", "")
         if class_name:
             classes.extend(class_name)
 
-        title_text, _ = "", ""
-        if "nonumber" in self.options:
-            title_text = f"{self.name.title()} "
+        title_text, title = "", ""
+        if self.name == "exercise":
+            if "nonumber" in self.options:
+                title_text = f"{self.name.title()} "
 
-        if self.arguments != []:
-            title_text += f"({self.arguments[0]})"
-            _ += self.arguments[0]
+            if self.arguments != []:
+                title_text += f"({self.arguments[0]})"
+                title += self.arguments[0]
+        else:
+            title_text = f"{self.name.title()} to "
+            target_label = self.arguments[0]
 
         textnodes, messages = self.state.inline_text(title_text, self.lineno)
 
         section = nodes.section(ids=[f"{self.name}-content"])
         self.state.nested_parse(self.content, self.content_offset, section)
 
-        if "nonumber" in self.options:
-            node = unenumerable_node()
+        if self.name == "exercise":
+            if "nonumber" in self.options:
+                node = unenumerable_node()
+            else:
+                node = enumerable_node()
         else:
-            node = enumerable_node()
+            node = linked_node()
 
         node += nodes.title(title_text, "", *textnodes)
         node += section
@@ -88,7 +88,11 @@ class ExerciseDirective(SphinxDirective):
         node["ids"].append(label)
         node["label"] = label
         node["docname"] = self.env.docname
+        node["hidden"] = True if "hidden" in self.options else False
         node.document = self.state.document
+
+        if self.name == "solution":
+            node["target_label"] = target_label
 
         self.add_name(node)
 
@@ -96,13 +100,34 @@ class ExerciseDirective(SphinxDirective):
             "type": self.name,
             "docname": self.env.docname,
             "node": node,
-            "title": _,
+            "title": title,
+            "hidden": node.get("hidden", bool),
         }
+
+        if node.get("hidden", bool):
+            return []
+
         return [node]
 
 
-class SolutionDirective(SphinxDirective):
-    """ A custom Sphinx Directive """
+class ExerciseDirective(CustomDirective):
+    """ A custom exercise directive """
+
+    name = "exercise"
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = True
+    option_spec = {
+        "label": directives.unchanged_required,
+        "class": directives.class_option,
+        "nonumber": directives.flag,
+        "hidden": directives.flag,
+    }
+
+
+class SolutionDirective(CustomDirective):
+    """ A custom solution directive """
 
     name = "solution"
     has_content = True
@@ -112,65 +137,5 @@ class SolutionDirective(SphinxDirective):
     option_spec = {
         "label": directives.unchanged_required,
         "class": directives.class_option,
+        "hidden": directives.flag,
     }
-    title_text = f"{name.title()} to "
-
-    def run(self):
-        serial_no = self.env.new_serialno()
-
-        if not hasattr(self.env, "exercise_list"):
-            self.env.exercise_list = {}
-
-        # Take care of class option
-        classes, class_name = [self.name], self.options.get("class", [])
-        if class_name:
-            classes.extend(class_name)
-
-        target_label = self.arguments[0]
-
-        textnodes, messages = self.state.inline_text(self.title_text, self.lineno)
-
-        section = nodes.section(ids=[f"{self.name}-content"])
-        self.state.nested_parse(self.content, self.content_offset, section)
-
-        node = linked_node()
-        node.document = self.state.document
-        node += nodes.title(self.title_text, "", *textnodes)
-        node += section
-
-        label = self.options.get("label", "")
-        if label:
-            self.options["noindex"] = False
-        else:
-            self.options["noindex"] = True
-            label = f"{self.env.docname}-{self.name}-{serial_no}"
-
-        # Duplicate label warning
-        if not label == "" and label in self.env.exercise_list.keys():
-            docpath = self.env.doc2path(self.env.docname)
-            path = docpath[: docpath.rfind(".")]
-            other_path = self.env.doc2path(self.env.exercise_list[label]["docname"])
-            msg = f"duplicate label: {label}; other instance in {other_path}"
-            logger.warning(msg, location=path, color="red")
-            return []
-
-        self.options["name"] = label
-
-        # Set node attributes
-        node["classes"].extend(classes)
-        node["ids"].append(label)
-        node["label"] = label
-        node["docname"] = self.env.docname
-        node["target_label"] = target_label
-        node.document = self.state.document
-
-        self.add_name(node)
-
-        self.env.exercise_list[label] = {
-            "type": self.name,
-            "docname": self.env.docname,
-            "node": node,
-            "title": "",
-        }
-
-        return [node]
