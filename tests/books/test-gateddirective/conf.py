701a39817a3a43fd0ee1e2c1e1f55711082a4a7b
mmcky <mamckay@gmail.com>
2022-03-08 13:57:54 +1100

    TST: Adding test for error cases

--- tests/books/test-gateddirective/conf.py
+++ tests/books/test-gateddirective/conf.py
@@ -35,7 +35,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["build", "errors*"]
+exclude_patterns = ["build", "_build", "errors_[1,2,3]*"]
 
 
 # -- Options for HTML output -------------------------------------------------
--- tests/test_gateddirective.py
+++ tests/test_gateddirective.py
@@ -1,6 +1,10 @@
-from bs4 import BeautifulSoup
+import os
 import pytest
 import shutil
+from bs4 import BeautifulSoup
+from sphinx.errors import ExtensionError
+
+from sphinx.testing.util import strip_escseq
 
 
 @pytest.mark.sphinx("html", testroot="gateddirective")
@@ -31,3 +35,58 @@ def test_gated_solution_doctree(app, docname, get_sphinx_app_doctree):
         resolve=False,
         regress=True,
     )
+
+
+# Errors
+
+
+def getwarning(warnings):
+    return strip_escseq(warnings.getvalue().replace(os.sep, "/"))
+
+
+@pytest.mark.sphinx("html", testroot="gateddirective")
+def test_gated_solution_errors_1(app, warning):
+    app.config.exclude_patterns = ["build", "_build", "errors_[2,3]*"]
+    try:
+        app.build()
+    except ExtensionError:
+        warnings = getwarning(warning)
+        warn1 = "is missing a solution-end directive"
+        warn2 = "solution-start at line: 20"
+        for warn in [warn1, warn2]:
+            assert warn in warnings
+        assert True
+    else:
+        assert False
+
+
+@pytest.mark.sphinx("html", testroot="gateddirective")
+def test_gated_solution_errors_2(app, warning):
+    app.config.exclude_patterns = ["build", "_build", "errors_[1,3]*"]
+    try:
+        app.build()
+    except ExtensionError:
+        warnings = getwarning(warning)
+        warn1 = "is missing a solution-start directive"
+        warn2 = "solution-end at line: 54"
+        for warn in [warn1, warn2]:
+            assert warn in warnings
+        assert True
+    else:
+        assert False
+
+
+@pytest.mark.sphinx("html", testroot="gateddirective")
+def test_gated_solution_errors_3(app, warning):
+    app.config.exclude_patterns = ["build", "_build", "errors_[1,2]*"]
+    try:
+        app.build()
+    except ExtensionError:
+        warnings = getwarning(warning)
+        warn1 = "contains nested solution-start and solution-end directives"
+        warn2 = "solution-start at line: 16\n  solution-start at line: 19\n  solution-end at line: 22\n  solution-end at line: 25"  # noqa: #501
+        for warn in [warn1, warn2]:
+            assert warn in warnings
+        assert True
+    else:
+        assert False
