a6767f0492c095f0b7c9b2c4441041aafe3a4e82
Vincent-Xavier JUMEL <vincent-xavier.jumel@ac-paris.fr>
2022-03-29 12:02:32 +0200

    Essai d'une nouvelle version
    Signed-off-by: Vincent-Xavier JUMEL <vincent-xavier.jumel@ac-paris.fr>

--- sphinx_exercise/nodes.py
+++ sphinx_exercise/nodes.py
@@ -7,12 +7,9 @@ Sphinx Exercise Nodes
 :copyright: Copyright 2020-2021 by the Executable Books team, see AUTHORS
 :licences: see LICENSE for details
 """
-<<<<<<< HEAD
 from sphinx.locale import get_translation
 from docutils.nodes import Node
-=======
 
->>>>>>> master
 from sphinx.util import logging
 from docutils.nodes import Node
 from docutils import nodes as docutil_nodes
@@ -21,13 +18,10 @@ from sphinx.writers.latex import LaTeXTranslator
 from .latex import LaTeXMarkup
 
 logger = logging.getLogger(__name__)
-<<<<<<< HEAD
 MESSAGE_CATALOG_NAME = "exercise"
 _ = get_translation(MESSAGE_CATALOG_NAME)
 
-=======
 LaTeX = LaTeXMarkup()
->>>>>>> master
 
 
 # Nodes
@@ -60,8 +54,8 @@ class solution_end_node(docutil_nodes.Admonition, docutil_nodes.Element):
 
 class exercise_title(docutil_nodes.title):
     def default_title(self):
-        title_text = self.children[0].astext()
-        if title_text == "Exercise" or title_text == "Exercise %s":
+        title_text = _(self.children[0].astext())
+        if title_text == _("Exercise") or title_text == "Exercise %s":
             return True
         else:
             return False
@@ -88,85 +82,7 @@ class exercise_latex_number_reference(sphinx_nodes.number_reference):
     pass
 
 
-<<<<<<< HEAD
-def _depart_nodes_latex(self, node, title, pop_index=False):
-    """ Function to handle depart_node for latex. """
-    idx = list_rindex(self.body, latex_admonition_start) + 2
-    if pop_index:
-        self.body.pop(idx)
-    self.body.insert(idx, title)
-    self.body.append(latex_admonition_end)
-
-
-def _remove_placeholder_title_exercise(typ, node):
-    """ Removing the exercise placeholder we put in title earlier."""
-    for title in node.traverse(docutil_nodes.title):
-        if typ.title() in title.astext():
-            title[0] = docutil_nodes.Text("")
-
-
-def visit_enumerable_node(self, node: Node) -> None:
-    typ = node.attributes.get("type", "")
-    if isinstance(self, LaTeXTranslator):
-        _remove_placeholder_title_exercise(typ, node)
-        _visit_nodes_latex(self, node, find_parent)
-    else:
-        _remove_placeholder_title_exercise(typ, node)
-        self.body.append(self.starttag(node, "div", CLASS="admonition"))
-
-
-def depart_enumerable_node(self, node: Node) -> None:
-    typ = node.attributes.get("type", "")
-    if isinstance(self, LaTeXTranslator):
-        number = get_node_number(self, node, typ)
-        _depart_nodes_latex(self, node, f"{_(typ.title())} {number} ")
-    else:
-        number = get_node_number(self, node, typ)
-        idx = list_rindex(self.body, f"{_(typ.title())} {number} ")
-        self.body[idx] = f"{_(typ.title())} {number} "
-        self.body.append("</div>")
-
-
-def visit_exercise_unenumerable_node(self, node: Node) -> None:
-    typ = node.attributes.get("type", "")
-    if isinstance(self, LaTeXTranslator):
-        _remove_placeholder_title_exercise(typ, node)
-        _visit_nodes_latex(self, node, find_parent)
-    else:
-        _remove_placeholder_title_exercise(typ, node)
-        self.body.append(self.starttag(node, "div", CLASS="admonition"))
-
-
-def depart_exercise_unenumerable_node(self, node: Node) -> None:
-    typ = node.attributes.get("type", "")
-    if isinstance(self, LaTeXTranslator):
-        _depart_nodes_latex(self, node, f"{_(typ.title())} ")
-    else:
-        idx = list_rindex(self.body, '<p class="admonition-title">') + 1
-        element = f"<span>{_(typ.title())} </span>"
-        self.body.insert(idx, element)
-        self.body.append("</div>")
-
-
-def visit_solution_node(self, node: Node) -> None:
-    if isinstance(self, LaTeXTranslator):
-        _visit_nodes_latex(self, node, find_parent)
-    else:
-        self.body.append(self.starttag(node, "div", CLASS="admonition"))
-
-
-def depart_solution_node(self, node: Node) -> None:
-    typ = node.attributes.get("type", "")
-    if isinstance(self, LaTeXTranslator):
-        _depart_nodes_latex(self, node, f"{_(typ.title())} to ", True)
-    else:
-        number = get_node_number(self, node, typ)
-        idx = list_rindex(self.body, f"{_(typ.title())} {number} ")
-        self.body.pop(idx)
-        self.body.append("</div>")
-=======
 # Test Node Functions
->>>>>>> master
 
 
 def is_exercise_node(node):
