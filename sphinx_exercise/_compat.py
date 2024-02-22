9f722f3a85c8fa5e4461a30ff17402524ab1cc8c
Angus Hollands <goosey15@gmail.com>
2024-02-22 15:18:04 +0000

    maint: add type hints

--- sphinx_exercise/_compat.py
+++ sphinx_exercise/_compat.py
@@ -1,8 +1,10 @@
-import docutils
+from docutils.nodes import Element
+from typing import Iterator
 
 
-def findall(node, *args, **kwargs):
+def findall(node: Element, *args, **kwargs) -> Iterator[Element]:
     # findall replaces traverse in docutils v0.18
     # note a difference is that findall is an iterator
     impl = getattr(node, "findall", node.traverse)
     return iter(impl(*args, **kwargs))
+
