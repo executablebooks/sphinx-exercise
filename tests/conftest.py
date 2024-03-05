3e983af69e4e78365acafbc56993551831e1024a
Angus Hollands <goosey15@gmail.com>
2024-03-05 12:14:46 +0000

    test: fix Sphinx 7 incompat

--- tests/conftest.py
+++ tests/conftest.py
@@ -86,11 +86,14 @@ def file_regression(file_regression):
 
 
 class FileRegression:
-    ignores = (
+    ignores = (    )
+    changes = (
         # TODO: Remove when support for Sphinx<=6 is dropped,
-        re.escape(" translation_progress=\"{'total': 0, 'translated': 0}\""),
+        (re.escape(" translation_progress=\"{'total': 0, 'translated': 0}\""), ""),
         # TODO: Remove when support for Sphinx<7.2 is dropped,
-        r"original_uri=\"[^\"]*\"\s",
+        (r"original_uri=\"[^\"]*\"\s", ""),
+        # TODO: Remove when support for Sphinx<7.2 is dropped
+        ("Link to", "Permalink to")
     )
 
     def __init__(self, file_regression):
@@ -100,6 +103,6 @@ class FileRegression:
         return self.file_regression.check(self._strip_ignores(data), **kwargs)
 
     def _strip_ignores(self, data):
-        for ig in self.ignores:
-            data = re.sub(ig, "", data)
+        for src, dst in self.changes:
+            data = re.sub(src, dst, data)
         return data
